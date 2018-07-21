79f165e65ed423310ce1714b6fdb93c373d8d3fc
felerminoali <felerminoali@gmail.com>
2018-07-21 21:11:50 +0200

    more attributes added to the conta view

--- src/main/java/municipio/service/CRUDServiceImpl.java
+++ src/main/java/municipio/service/CRUDServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import esira.dao.CRUDDao;
+import municipio.dao.CRUDDao;
 import java.io.Serializable;
 import java.util.Map;
 import org.hibernate.LockMode;
