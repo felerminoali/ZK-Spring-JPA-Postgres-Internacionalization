94932bb7e599af329af86db702b149b2d2c504e7
felerminoali <felerminoali@gmail.com>
2018-07-23 21:31:45 +0200

    modifying beans

---
+++ src/main/java/municipio/hibernate/Login.java
@@ -0,0 +1,15 @@
+package municipio.hibernate;
+
+public class Login {
+	
+	private static String tenantId="fecn1";
+
+	public static String getTenantId() {
+		return tenantId;
+	}
+
+	public static void setTenantId(String myTenantId) {
+		tenantId = myTenantId;
+	}
+
+}
