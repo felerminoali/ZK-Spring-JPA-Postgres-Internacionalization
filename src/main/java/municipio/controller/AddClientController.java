e14bb0eb48a5612e7c5e8230f58f237b9dd14170
felerminoali <felerminoali@gmail.com>
2018-07-30 21:32:58 +0200

    interfce editing

--- src/main/java/municipio/controller/AddClientController.java
+++ src/main/java/municipio/controller/AddClientController.java
@@ -13,42 +13,42 @@ import java.util.List;
 public class AddClientController extends SelectorComposer<Component> {
 
 
-    @Wire("#grpData")
-    Div grpData, grpOtherData;
-
-    @Wire
-    Include idInclData, idInclOtherData;
-
-    @Wire
-    private Combobox cmbLegalentitytype;
+//    @Wire("#grpData")
+//    Div grpData, grpOtherData;
+//
+//    @Wire
+//    Include idInclData, idInclOtherData;
+//
+//    @Wire
+//    private Combobox cmbLegalentitytype;
 
     @Override
     public void doAfterCompose(Component comp) throws Exception{
         super.doAfterCompose(comp);
     }
 
-
-    public ListModel<String> getEntidadeLegalModel() {
-        List<String> entidadeLegal = new ArrayList<String>();
-
-        // Get data from Database
-        entidadeLegal.add("Pessoa Singular");
-        entidadeLegal.add("Organização");
-
-        return new ListModelList<String>(entidadeLegal);
-    }
-
-    @Listen("onChange = combobox#cmbLegalentitytype")
-    public void change() {
-       String str = cmbLegalentitytype.getSelectedItem().getValue();
-        if(str.equals("Pessoa Singular")) {
-            grpData.setVisible(true);
-            idInclData.setSrc("conta/clientdata.zul");
-        }else if(str.equals("Organização")){
-            grpData.setVisible(true);
-            idInclData.setSrc("conta/orgdata.zul");
-        }
-    }
+//
+//    public ListModel<String> getEntidadeLegalModel() {
+//        List<String> entidadeLegal = new ArrayList<String>();
+//
+//        // Get data from Database
+//        entidadeLegal.add("Pessoa Singular");
+//        entidadeLegal.add("Organização");
+//
+//        return new ListModelList<String>(entidadeLegal);
+//    }
+//
+//    @Listen("onChange = combobox#cmbLegalentitytype")
+//    public void change() {
+//       String str = cmbLegalentitytype.getSelectedItem().getValue();
+//        if(str.equals("Pessoa Singular")) {
+//            grpData.setVisible(true);
+//            idInclData.setSrc("conta/clientdata.zul");
+//        }else if(str.equals("Organização")){
+//            grpData.setVisible(true);
+//            idInclData.setSrc("conta/orgdata.zul");
+//        }
+//    }
 
 
 
